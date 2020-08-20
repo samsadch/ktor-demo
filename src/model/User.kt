fdcac698a7ff280671ad0ef3d35ea99c00bff6a7
Samsad CV <samsadch@gmail.com>
2020-08-20 13:08:20 +0400

    Create User.kt

---
+++ src/model/User.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+class User(val name: String, val password: String)
+
+val users = Collections.synchronizedMap(
+    listOf(User("test", "test"))
+        .associateBy { it.name }
+        .toMutableMap()
+)
+
+class LoginRegister(val user: String, val password: String)
